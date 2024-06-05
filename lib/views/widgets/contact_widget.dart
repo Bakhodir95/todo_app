970605cced9323bd41acd4b1ada11d1418c00ca9
Bakhodir <106083101+Bakhodir95@users.noreply.github.com>
2024-06-05 16:05:52 +0500

    w

--- lib/views/widgets/contact_widget.dart
+++ lib/views/widgets/contact_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:todo_app/controllers/contact_controller.dart';
 
 class ContactWidget extends StatefulWidget {
@@ -10,15 +11,43 @@ class ContactWidget extends StatefulWidget {
 
 class _HomePageState extends State<ContactWidget> {
   ContactController controller = ContactController();
-
+  final nameController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Contact Page"),
+        title: const Text(
+          "Contact Page",
+          style: TextStyle(fontSize: 30),
+        ),
         centerTitle: true,
         backgroundColor: Colors.blue,
         foregroundColor: Colors.white,
+        actions: [
+          IconButton(
+              onPressed: () {
+                showDialog(
+                    context: context,
+                    builder: (ctx) {
+                      return AlertDialog(
+                        title: Text("Add Contacts"),
+                        content: Column(
+                          children: [
+                            TextField(
+                              controller: nameController,
+                              decoration:
+                                  InputDecoration(border: OutlineInputBorder()),
+                            )
+                          ],
+                        ),
+                      );
+                    });
+              },
+              icon: const Icon(
+                Icons.add,
+                size: 40,
+              ))
+        ],
       ),
       body: FutureBuilder(
         future: controller.getContacts(),
@@ -37,7 +66,10 @@ class _HomePageState extends State<ContactWidget> {
                   itemCount: contacts.length,
                   itemBuilder: (ctx, index) {
                     return ListTile(
-                      title: Text("${index + 1}.${contacts[index].name}"),
+                      title: Text(
+                        "${index + 1}.${contacts[index].name}",
+                        style: const TextStyle(fontSize: 30),
+                      ),
                     );
                   },
                 );
